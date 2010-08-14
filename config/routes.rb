38787fc328614a5a8dbe88ec3c7cd2c7c8a0f155
Brian <brianfoshee@gmail.com>
2010-08-14 18:42:04 -0400

    removed the sign_up URL - don not want anyone to sign up, manual user creation only

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ActionController::Routing::Routes.draw do |map|
     
   map.resources :sermons  
 
+  map.sign_up '/sign_up', :controller => 'sermons', :action => 'index'
+  
   Clearance::Routes.draw(map)
   CaptainOveur::Routes.draw(map)
   
