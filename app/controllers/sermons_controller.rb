27b9316ac5f27fb5a24d51e9d6cd7d818fd479cd
Brian <brianfoshee@gmail.com>
2010-08-14 00:23:33 -0400

    minor details

--- app/controllers/sermons_controller.rb
+++ app/controllers/sermons_controller.rb
@@ -1,5 +1,5 @@
 class SermonsController < ApplicationController
-  before_filter :authenticate, :only => [ :update, :destroy, :edit ]
+  before_filter :authenticate, :only => [ :update, :destroy, :edit, :new ]
   
   # GET /sermons
   # GET /sermons.xml
